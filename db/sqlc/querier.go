aecdc18efea45ff74f1abe3b887d2999f554ffc9
ot07 <ot07neoki@gmail.com>
2023-02-12 20:38:11 +0900

    add delete members function

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -13,6 +13,7 @@ import (
 type Querier interface {
 	CreateMember(ctx context.Context, arg CreateMemberParams) (Member, error)
 	DeleteMember(ctx context.Context, id uuid.UUID) error
+	DeleteMembers(ctx context.Context, dollar_1 []uuid.UUID) error
 	GetMember(ctx context.Context, id uuid.UUID) (Member, error)
 	ListMembers(ctx context.Context, arg ListMembersParams) ([]Member, error)
 	UpdateMember(ctx context.Context, arg UpdateMemberParams) (Member, error)
