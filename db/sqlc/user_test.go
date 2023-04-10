5a36d8bf5d6b167269a36a3cd474d6f7d65d57c8
ot07 <ot07neoki@gmail.com>
2023-04-10 22:57:08 +0900

    add user.sql

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ot07/coworker-backend/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomUser(t *testing.T, testQueries *Queries) User {
+	arg := CreateUserParams{
+		FirstName:      util.RandomName(),
+		LastName:       util.RandomName(),
+		Email:          util.RandomEmail(),
+		HashedPassword: "secret",
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.FirstName, user.FirstName)
+	require.Equal(t, arg.LastName, user.LastName)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+
+	require.NotEmpty(t, user.ID)
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	t.Parallel()
+
+	tx := beginTransaction(t)
+	defer rollbackTransaction(t, tx)
+
+	testQueries := New(tx)
+
+	createRandomUser(t, testQueries)
+}
+
+func TestGetUser(t *testing.T) {
+	t.Parallel()
+
+	tx := beginTransaction(t)
+	defer rollbackTransaction(t, tx)
+
+	testQueries := New(tx)
+
+	user1 := createRandomUser(t, testQueries)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.FirstName, user2.FirstName)
+	require.Equal(t, user1.LastName, user2.LastName)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
